10fb3409efe5399d8c15768b05c61b0d684d60b3
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-09-09 16:43:11 +0300

    add callBackHandler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Component1 from "./components/Component1";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [allGreen, setAllGreen] = useState(false);
+
+    const handleFinalCallback = () => {
+        setAllGreen(true);
+    };
+
+    return (
+        <div className="App">
+            <h1>Callback Hell</h1>
+            <div className="container">
+                <Component1 callBack={handleFinalCallback}/>
+            </div>
+            {allGreen && <p>Первый круг Ада пройден!</p>}
+        </div>
+    );
 }
+
 
 export default App;
